239c48a6b57984f1699def4a3a438530fdac2600
Fagner202 <fagnersilveira86@gmail.com>
2025-06-16 16:19:35 -0300

    Atualiza a view de barbeiros: ajusta a renderização da lista de barbeiros, inclui informações do usuário autenticado e remove código comentado desnecessário.

--- src/views/barbeiros/index.php
+++ src/views/barbeiros/index.php
@@ -1,17 +1,55 @@
 <?php
 session_start();
-// require_once __DIR__ . '/../utils/utils.php';
-// require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../../utils/utils.php';
+
 $usuario = autenticarUsuario();
-$title = 'Páigna de teste de rotas';
-$cssPage = 'agendamentos'; // Para carregar o CSS específico
+$title = 'Gerenciar Barbeiros';
+$cssPage = ''; // Nenhum CSS específico por enquanto
 
 ob_start();
 ?>
 
-<div class="row">
-    <h1>Aqui será a index para Barbeiros</h1>
-</div>
+<h1>Lista de Barbeiros</h1>
+<p>Usuário autenticado: <?= htmlspecialchars($usuario['nome'] ?? 'Desconhecido') ?></p>
+
+<a href="/barbeiros/novo">Cadastrar Novo Barbeiro</a>
+
+<?php if (!empty($barbeiros)) : ?>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Idade</th>
+                <th>Data de Contratação</th>
+                <th>Especialidades</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($barbeiros as $barbeiro) : ?>
+                <tr>
+                    <td><?= htmlspecialchars($barbeiro['id']) ?></td>
+                    <td><?= htmlspecialchars($barbeiro['nome']) ?></td>
+                    <td><?= htmlspecialchars($barbeiro['email']) ?></td>
+                    <td><?= htmlspecialchars($barbeiro['idade']) ?></td>
+                    <td><?= htmlspecialchars($barbeiro['data_contratacao']) ?></td>
+                    <td><?= htmlspecialchars($barbeiro['especialidades'] ?? 'Nenhuma') ?></td>
+                    <td>
+                        <a href="/barbeiros/editar?id=<?= $barbeiro['id'] ?>">Editar</a> |
+                        <a href="/barbeiros/deletar?id=<?= $barbeiro['id'] ?>" onclick="return confirm('Tem certeza que deseja excluir este barbeiro?')">Excluir</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php else : ?>
+    <p>Nenhum barbeiro cadastrado.</p>
+<?php endif; ?>
+
+<p><a href="/agendamentos">Voltar ao Dashboard</a></p>
+
 <?php
 $content = ob_get_clean();
 require __DIR__ . '/../layouts/auth_layout.php';
