47b9e46aff33e3fab3d34b6d86b53baa5b7f208a
Fagner202 <fagnersilveira86@gmail.com>
2025-06-17 20:42:58 -0300

    Atualiza a visualização da lista de barbeiros para incluir o status e remove código de depuração.

--- src/views/agendamento/index.php
+++ src/views/agendamento/index.php
@@ -4,15 +4,34 @@ require_once __DIR__ . '/../../utils/utils.php';
 
 $usuario = autenticarUsuario();
 
-// dd($barbeiros);
-
 ob_start();
 ?>
 
+<h1>Lista de Barbeiros</h1>
 
+<ul>
+    <?php foreach ($barbeiros as $barbeiro): ?>
+        <li>
+            ID do Cliente: <?= htmlspecialchars($barbeiro['cliente_id']) ?><br>
+            Idade: <?= htmlspecialchars($barbeiro['idade']) ?><br>
+            Data de Contratação: <?= htmlspecialchars($barbeiro['data_contratacao']) ?><br>
+            Status: <?= htmlspecialchars($barbeiro['status']) ?><br>
+
+            <button onclick="mostrarEspecialidades(<?= $barbeiro['cliente_id'] ?>)">Especialidades</button>
+        </li>
+        <hr>
+    <?php endforeach; ?>
+</ul>
+
+<!-- Função JavaScript -->
+<script>
+    function mostrarEspecialidades(clienteId) {
+        alert("Mostrar especialidades para o barbeiro com ID: " + clienteId);
+        // Aqui você pode futuramente fazer uma chamada AJAX ou redirecionar para outra página
+    }
+</script>
 
 <?php
 $content = ob_get_clean();
 require __DIR__ . '/../layouts/auth_layout.php';
 ?>
-
